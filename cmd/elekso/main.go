dd5606dea57283c94a16c2053da32f4639ae4f89
Mandar <01.mandar@gmail.com>
2016-04-12 08:41:51 +0530

    h9

--- cmd/elekso/main.go
+++ cmd/elekso/main.go
@@ -83,7 +83,7 @@ func main() {
 
 
    fmt.Sprintf("name: %s; data: %s time: %s",  json.P_User , json.P_Data,json.P_Time)
-     c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+    // c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
  })
 	
 	
